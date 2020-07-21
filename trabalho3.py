ebc2b76822e8fbbdb788254ccd1d9f0f380ce2c3
MSCapeletti <matheus_capeletti@hotmail.com>
2020-07-21 08:42:24 -0300

    Data cleaning and plotly testing

--- trabalho3.py
+++ trabalho3.py
@@ -1,5 +1,9 @@
-import pandas as pd
 import sys
+import math
+
+import pandas as pd
+import plotly.express as px
+from sklearn.linear_model import LinearRegression
 
 
 if __name__ == '__main__':
@@ -10,4 +14,19 @@ if __name__ == '__main__':
         print("Provide the csv with historical data for analysis")
         sys.exit()
 
-    print(data.head())
+    # Drop NaN rows
+    data = data.dropna()
+
+    # Reset index so that there are no missing indexes from the removed rows
+    data = data.reset_index(drop=True)
+
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    data['Variation'] = data['Close'].sub(data['Open'])
+
+    #print(data.head())
+
+    x1=data.Date
+    y1=data.Close
+    fig = px.scatter(data, x='Date', y='Close')
+    fig.show()
