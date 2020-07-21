c76ae5312fcb420113bdbfeb0148171dc59014b0
MSCapeletti <matheus_capeletti@hotmail.com>
2020-07-21 09:21:35 -0300

    LinearRegression using Open, High, Low, Date_delta. The regression is made using the entire data

--- trabalho3.py
+++ trabalho3.py
@@ -1,6 +1,7 @@
 import sys
 import math
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
@@ -22,11 +23,22 @@ if __name__ == '__main__':
 
     data['Date'] = pd.to_datetime(data['Date'])
 
+    data['Date_delta'] = (data['Date'] - data['Date'].min()) / np.timedelta64(1, 'D')
+
     data['Variation'] = data['Close'].sub(data['Open'])
 
     #print(data.head())
 
-    x1=data.Date
-    y1=data.Close
-    fig = px.scatter(data, x='Date', y='Close')
-    fig.show()
+    # x1=data.Date
+    # y1=data.Close
+    # fig = px.scatter(data, x='Date', y='Close')
+    # fig.show()
+
+    x_v = data[['Open', 'High', 'Low', 'Date_delta']]
+    y_v = data[['Close']]
+
+    model = LinearRegression()
+    model.fit(x_v, y_v)
+
+    print("Coefficients and constant found:")
+    print(model.coef_, model.intercept_)
