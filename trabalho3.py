2907c6ebe0dc796972432db5ac92d9dd4927203f
MSCapeletti <matheus_capeletti@hotmail.com>
2020-07-28 18:27:25 -0300

    Training set and Validation set, Report template in LateX

--- trabalho3.py
+++ trabalho3.py
@@ -29,16 +29,30 @@ if __name__ == '__main__':
 
     #print(data.head())
 
-    # x1=data.Date
-    # y1=data.Close
-    # fig = px.scatter(data, x='Date', y='Close')
-    # fig.show()
+    print("Correlation between Date and Closing values:")
+    print(data[['Date_delta', 'Close']].corr())
 
-    x_v = data[['Open', 'High', 'Low', 'Date_delta']]
-    y_v = data[['Close']]
+    x1=data.Date
+    y1=data.Close
+    fig = px.scatter(data, x='Date', y='Close')
+    fig.show()
+
+    maxDate = data['Date_delta'].max()
+
+    trainingSet = data[data['Date_delta'] < round(maxDate*0.8)]
+    validationSet = data[data['Date_delta'] >= round(maxDate*0.8)] 
+
+    x_v = trainingSet[['Date_delta']]
+    y_v = trainingSet[['Close']]
 
     model = LinearRegression()
     model.fit(x_v, y_v)
 
-    print("Coefficients and constant found:")
-    print(model.coef_, model.intercept_)
+    # print("Coefficients and constant found:")
+    # print(model.coef_, model.intercept_)
+
+    # print("Real values for the period provided, predicted values for the same period:")
+    # print(y_v, model.predict(x_v))
+
+    print("model score:")
+    print(model.score(x_v, y_v))
