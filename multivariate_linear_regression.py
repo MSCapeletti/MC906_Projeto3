03bbcd403ddbd4ea825c78d6a19b3a27453ace7a
Lucas Barretto Andrade <lucasbarretto@hotmail.com>
2020-08-09 00:13:44 -0300

    Try predict future dates with multivariate linear model

--- multivariate_linear_regression.py
+++ multivariate_linear_regression.py
@@ -11,6 +11,61 @@ from datetime import datetime
 import visuals_and_graphics as vag
 from process import process
 
+def adjustDateForModel(X, Y):
+    # use last day's values to predict next day's close
+    # remove first day from target
+    Y = Y.drop([0])
+    Y = Y.reset_index(drop=True)
+
+    # remove the last day from input
+    X = X.drop([X.shape[0] - 1])
+    X = X.reset_index(drop=True)
+    return X, Y
+
+def fittedModel(trainData, features, target):
+    model = LinearRegression()
+
+    X = trainData[features]
+    Y = trainData[target]
+
+    X, Y = adjustDateForModel(X, Y)
+
+    model.fit(X, Y)
+
+    return model
+
+def predictNextDates(lastDayData, dates):
+    featuresModels = dict()
+    for target in features:
+        newModel = fittedModel(trainData, features, target)
+        featuresModels[target] = newModel
+
+    predictedData = pd.DataFrame(columns=data.columns)
+    predictedData = predictedData.append(lastDayData)
+    idx = 1
+    while predictedData.shape[0] < dates.shape[0]:
+        lastDayFeatures = predictedData[features].tail(1)
+        open_value = featuresModels['Open'].predict(lastDayFeatures)
+        close_value = featuresModels['Close'].predict(lastDayFeatures)
+        low_value = featuresModels['Low'].predict(lastDayFeatures)
+        high_value = featuresModels['High'].predict(lastDayFeatures)
+        volume_value = featuresModels['Volume'].predict(lastDayFeatures)
+        date = dates[idx]
+
+        predictedData = predictedData.append({
+            'Date': date,
+            'Open': open_value[0],
+            'Close': close_value[0],
+            'Low': low_value[0],
+            'High': high_value[0],
+            'Volume': volume_value[0]
+        }, ignore_index=True)
+
+        idx = idx + 1
+
+    predictedData = predictedData.drop([0])
+
+    return predictedData
 
 if __name__ == '__main__':
 
@@ -37,11 +92,6 @@ if __name__ == '__main__':
     print("Correlation between data")
     print(data[['Date_delta', 'Close', 'Open', 'High', 'Low', 'Volume']].corr())
 
-    # x1=data.Date
-    # y1=data.Close
-    # fig = px.scatter(data, x='Date', y='Close')
-    # fig.show()
-
     maxDate = data['Date_delta'].max()
 
     features = ['Open', 'High', 'Low', 'Volume', 'Close']
@@ -51,17 +101,10 @@ if __name__ == '__main__':
 
     X = trainData[features]
     Y = trainData['Close']
-    # use last day's values to predict next day's close
-    # remove first day from target
-    Y = Y.drop([0])
-    Y = Y.reset_index(drop=True)
+    
+    X, Y = adjustDateForModel(X, Y)
 
-    # remove the last day from input
-    X = X.drop([X.shape[0] - 1])
-    X.reset_index(drop=True)
-
-    model = LinearRegression()
-    model.fit(X, Y)
+    model = fittedModel(trainData, features, 'Close')
 
     predicted_data = pd.DataFrame(columns=['Date', 'Close'])
     closingValues = model.predict(validationData[features])
@@ -75,4 +118,20 @@ if __name__ == '__main__':
 
     # Grafico com os Close históricos e previstos para as mesmas datas
     vag.fig_real_predicted_values(
-        data, predicted_data, 'Comparacao entre previsão e real, previsao feita com os dados historicos anteriores')
+        validationData, predicted_data, 'Comparação entre o histórico e a predição realizada pelo modelo.')
+    
+
+    # -------------------------------------------------------------------------------------------
+    # try a prediction of more days by predicting each value conseuctively
+    # -------------------------------------------------------------------------------------------
+    predicted_data = predictNextDates(validationData.head(1), validationData['Date'].values)
+    vag.fig_real_predicted_values(
+        validationData, predicted_data, 'Comparação entre o histórico e a predição realizada pelo modelo.')
+
+    future_dates = np.array(validationData['Date'].tail(1).values)
+    for i in range(20):
+        future_dates = np.append(future_dates, future_dates[i] + np.timedelta64(1, 'D'))
+
+    predicted_data = predictNextDates(validationData.tail(1), future_dates)
+    vag.fig_real_predicted_values(
+        validationData, predicted_data, 'Comparação entre o histórico e a predição realizada pelo modelo.')
